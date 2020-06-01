cb660c7f498eb44ebf6f1172e599d50f4c1606d9
dvdlvc <63413459+dvdlvc@users.noreply.github.com>
2020-06-01 10:14:24 +0200

    Update Sadd_Exp.R

--- Sadd_Exp.R
+++ Sadd_Exp.R
@@ -4,29 +4,28 @@
 # by Ilievski, La Vecchia and Ronchetti (2019).
 
 
-library('EQL')
+library('EQL') # library containing Edgeworth and saddlepoint density approximations
 
 ## Fig 2
 
 df <-1
 shape <- scale <- 1 # exponential random variable with rate = 1
 sigma2 <- 1
-rho3 <- 2
-rho4 <- 6
-mu <- 1
+# higher order moments of order 3 and 4
+rho3 <- 2 
+rho4 <- 6 
+mu <- 1 # mean of the asym approx
 tail <- 3 # define a cut off for the tail---needed just for plotting purposes
 
 
-# Approximations 
+# Approximations: functional forms 
 n <- 10 # 
 x <- seq(max(df-tail*sqrt(2*df/n),0), df+tail*sqrt(2*df/n), length=1000) # grid of points
 ea <- edgeworth(x, n, rho3, rho4, mu, sigma2, type="mean") # Edg expansion of the mean
 true_dens <- n*dgamma(n*x, shape=n*shape, scale=scale)     # true density of the mean      
 
 
-
-
-# Code for Fig 2
+# Plot in Fig 2
 plot(x,true_dens,col=1,type='l',lty=2, lwd=2, xlab="z", ylab="Density")
 lines(x,ea$approx,col=1,lty=1,lwd=2)
 lines(x,asy,col=2,lty=4,lwd=2)
@@ -35,7 +34,7 @@ legend(x=1.5,y=1,c('true','Edg','asy'), col=c(1,1,2), lwd=c(2,2,2),
 
 
 
-## Code for Fig 3
+## Plot in Fig 3
 
 # Approximations for a fix n (code below for n = 10, change n to obtain the other plots)
 
